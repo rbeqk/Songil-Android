51e5a1843522cc02e62c463c1b4d02132f4d9324
YSH <sh10302@naver.com>
2021-10-26 22:00:56 +0900

    add remain login layout

---
+++ app/src/main/java/com/example/songil/page_login/LoginFragment2.kt
@@ -0,0 +1,15 @@
+package com.example.songil.page_login
+
+import android.os.Bundle
+import android.view.View
+import com.example.songil.R
+import com.example.songil.config.BaseFragment
+import com.example.songil.databinding.LoginFragment2Binding
+
+class LoginFragment2(private val viewModel: LoginViewModel) : BaseFragment<LoginFragment2Binding>(LoginFragment2Binding::bind, R.layout.login_fragment_2) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
