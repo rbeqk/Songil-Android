cd038ce413bf0835b369385024fad7fdd3f810aa
YSH <sh10302@naver.com>
2021-10-29 22:00:07 +0900

    base setting

--- app/src/main/java/com/example/songil/config/GlobalApplication.kt
+++ app/src/main/java/com/example/songil/config/GlobalApplication.kt
@@ -2,15 +2,34 @@ package com.example.songil.config
 
 import android.app.Application
 import android.content.SharedPreferences
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class GlobalApplication : Application() {
     companion object {
-        val API_URL = ""
+        val API_URL = "https://dev.songil.shop/"
+        const val X_ACCESS_TOKEN = "x-access-token"
+        lateinit var sRetrofit: Retrofit
         lateinit var globalSharedPreferences: SharedPreferences
     }
 
     override fun onCreate() {
         super.onCreate()
+        initRetrofitInstance()
         globalSharedPreferences = applicationContext.getSharedPreferences("Songil", MODE_PRIVATE)
+    }
+
+    private fun initRetrofitInstance() {
+        val client : OkHttpClient = OkHttpClient.Builder()
+                .readTimeout(5000, TimeUnit.MILLISECONDS)
+                .connectTimeout(5000, TimeUnit.MILLISECONDS)
+                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)) //이건 log 부분이므로 앱 출시 후 삭제할것
+                .build()
+
+        sRetrofit = Retrofit.Builder().baseUrl(API_URL).client(client).addConverterFactory(GsonConverterFactory.create()).build()
+
     }
 }
