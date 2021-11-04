878d1d2d027cac8e6f448b5b24029fa7eed33d48
YSH <sh10302@naver.com>
2021-11-04 19:48:51 +0900

    add ask fragment in craft

---
+++ app/src/main/java/com/example/songil/page_craft/subpage_ask/CraftFragmentAsk.kt
@@ -0,0 +1,9 @@
+package com.example.songil.page_craft.subpage_ask
+
+import com.example.songil.R
+import com.example.songil.config.BaseFragment
+import com.example.songil.databinding.CraftFragmentAskBinding
+
+class CraftFragmentAsk : BaseFragment<CraftFragmentAskBinding>(CraftFragmentAskBinding::bind, R.layout.craft_fragment_ask) {
+
+}
