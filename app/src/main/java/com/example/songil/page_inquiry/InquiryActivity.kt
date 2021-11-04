87332dad53bbd7fd6b5df99bd3429be35ee14ca7
YSH <sh10302@naver.com>
2021-11-04 19:46:18 +0900

    add inquiry page and popup

---
+++ app/src/main/java/com/example/songil/page_inquiry/InquiryActivity.kt
@@ -0,0 +1,8 @@
+package com.example.songil.page_inquiry
+
+import com.example.songil.R
+import com.example.songil.config.BaseActivity
+import com.example.songil.databinding.InquiryActivityBinding
+
+class InquiryActivity : BaseActivity<InquiryActivityBinding>(R.layout.inquiry_activity) {
+}
---
+++ app/src/main/java/com/example/songil/popup_inquiry/InquiryDialog.kt
@@ -0,0 +1,38 @@
+package com.example.songil.popup_inquiry
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import androidx.fragment.app.DialogFragment
+import com.example.songil.databinding.InquiryPopupConfirmBinding
+
+class InquiryDialog : DialogFragment() {
+    private var _binding : InquiryPopupConfirmBinding ?= null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = InquiryPopupConfirmBinding.inflate(LayoutInflater.from(context))
+
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnClose.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
