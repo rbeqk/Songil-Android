e282b255c87149d2b838e7dd5202226936cc0330
YSH <sh10302@naver.com>
2021-11-08 21:17:52 +0900

    add seekBar (used when viewpager2 scroll event)

--- app/src/main/java/com/example/songil/page_article/ArticleFragmentMain.kt
+++ app/src/main/java/com/example/songil/page_article/ArticleFragmentMain.kt
@@ -1,9 +1,12 @@
 package com.example.songil.page_article
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.SeekBar
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.widget.ViewPager2
 import com.example.songil.R
 import com.example.songil.recycler.adapter.ArticleViewPager2Adapter
 import com.example.songil.recycler.decoration.ArticleViewPager2ItemDecoration
@@ -17,12 +20,29 @@ import kotlin.math.abs
 class ArticleFragmentMain : BaseFragment<ArticleFragmentMainBinding>(ArticleFragmentMainBinding::bind, R.layout.article_fragment_main) {
 
     private lateinit var viewModel: ArticleViewModel
+    private var isUser = false
+
+    private var pageRangeSize = 1
+    private var progressMax = 1
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = ViewModelProvider(this)[ArticleViewModel::class.java]
 
+        setViewPager()
+        setSeekBar()
+
+        val articleObserver = Observer<ArrayList<Article>>{ LiveData ->
+            (binding.vp2Article.adapter as ArticleViewPager2Adapter).applyData(LiveData)
+            pageRangeSize = (progressMax / (LiveData.size - 1))
+        }
+
+        viewModel.articleData.observe(viewLifecycleOwner, articleObserver)
+        viewModel.getArticleData()
+    }
+
+    private fun setViewPager(){
         val nextItemVisiblePx = dpToPx(activity as MainActivity, 16)
         val currentItemHorizontalMarginPx = dpToPx(activity as MainActivity, 32)
         val pageTranslationX = nextItemVisiblePx + currentItemHorizontalMarginPx
@@ -33,12 +53,34 @@ class ArticleFragmentMain : BaseFragment<ArticleFragmentMainBinding>(ArticleFrag
         }
         binding.vp2Article.addItemDecoration(ArticleViewPager2ItemDecoration(activity as MainActivity))
         binding.vp2Article.offscreenPageLimit = 3
+        binding.vp2Article.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if (!isUser){
+                    binding.seekArticle.setProgress(position * pageRangeSize, true)
+                }
+            }
+        })
+    }
 
-        val articleObserver = Observer<ArrayList<Article>>{ LiveData ->
-            (binding.vp2Article.adapter as ArticleViewPager2Adapter).applyData(LiveData)
-        }
+    private fun setSeekBar(){
+        progressMax = binding.seekArticle.max
+        binding.seekArticle.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser){
+                    isUser = true
+                    binding.vp2Article.currentItem = (seekBar!!.progress) / pageRangeSize
+                }
+            }
 
-        viewModel.articleData.observe(viewLifecycleOwner, articleObserver)
-        viewModel.getArticleData()
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+/*                val threshold = (seekBar!!.progress / pageRangeSize) * pageRangeSize
+                binding.seekArticle.setProgress(threshold, true)*/
+                isUser = false
+            }
+
+        })
     }
 }
