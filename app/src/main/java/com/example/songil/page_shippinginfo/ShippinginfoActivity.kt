97970295cff5265d25d1e1b985773aaea95fb0ad
YSH <sh10302@naver.com>
2021-11-27 23:21:23 +0900

    add layout (input shipping info, no product, artist manage page, answer page, + some items)

---
+++ app/src/main/java/com/example/songil/page_shippinginfo/ShippinginfoActivity.kt
@@ -0,0 +1,8 @@
+package com.example.songil.page_shippinginfo
+
+import com.example.songil.R
+import com.example.songil.config.BaseActivity
+import com.example.songil.databinding.ShippinginfoActivityInputBinding
+
+class ShippinginfoActivity : BaseActivity<ShippinginfoActivityInputBinding>(R.layout.shippinginfo_activity_input) {
+}
