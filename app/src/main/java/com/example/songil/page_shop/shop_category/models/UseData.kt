728bb81fefc952c45f362f480094a64c35a7992b
YSH <sh10302@naver.com>
2021-12-16 14:50:06 +0900

    fix data type

--- app/src/main/java/com/example/songil/page_shop/shop_category/models/UseData.kt
+++ app/src/main/java/com/example/songil/page_shop/shop_category/models/UseData.kt
@@ -2,7 +2,9 @@ package com.example.songil.page_shop.shop_category.models
 
 data class CraftSimple(val productIdx : Int, val productName : String, val thumbnailImg : String, val price : Int, val artistName : String)
 
-data class CraftDetail(val productIdx : Int, val productName : String, val thumbnailImg : String, val likeCount : Int, val reviewCount : Int,
-                       val price : Int, val artistName : String, val newOrNot : String, val likeOrNot : Int)
+data class CraftDetail(val productIdx : Int, val productName : String, val thumbnailImg : String, var likeCount : Int, var reviewCount : Int,
+                       val price : Int, val artistName : String, val newOrNot : String, var likeOrNot : Int)
 
 data class ProductAllResult(val popularList : ArrayList<CraftSimple>, val productsList : ArrayList<CraftDetail>)
+
+data class LikeData(val isLike : Boolean, val totalLikeCnt : Int, val itemPosition : Int) // itemPosition is item position in recyclerView Adapter
