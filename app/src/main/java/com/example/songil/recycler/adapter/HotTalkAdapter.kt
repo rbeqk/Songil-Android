ebfc8f41cb0216ec09b5882e2b087c4d8f29a91f
YSH <sh10302@naver.com>
2022-02-08 22:10:28 +0900

    connect hot-talk recyclerView and Qna, Ab-test page

--- app/src/main/java/com/example/songil/recycler/adapter/HotTalkAdapter.kt
+++ app/src/main/java/com/example/songil/recycler/adapter/HotTalkAdapter.kt
@@ -1,12 +1,17 @@
 package com.example.songil.recycler.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.songil.config.BaseActivity
+import com.example.songil.config.GlobalApplication
 import com.example.songil.data.HotTalk
 import com.example.songil.databinding.ItemHottalkAbtestBinding
 import com.example.songil.databinding.ItemHottalkQnaBinding
+import com.example.songil.page_abtest.AbtestActivity
+import com.example.songil.page_qna.QnaActivity
 
 class HotTalkAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
@@ -17,11 +22,13 @@ class HotTalkAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         val category = binding.tvCategory
         val title = binding.tvTitle
         val commentCount = binding.tvCommentCount
+        val root = binding.root
     }
     class HotTalkAbTestViewHolder(binding : ItemHottalkAbtestBinding) : RecyclerView.ViewHolder(binding.root){
         val category = binding.tvCategory
         val artistImage = binding.ivProfile
         val artistName = binding.tvArtistName
+        val root = binding.root
     }
 
     override fun getItemViewType(position: Int): Int {
@@ -48,11 +55,21 @@ class HotTalkAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
                 (holder as HotTalkQnaViewHolder).category.text = category[hotTalkData[position].categoryIdx]
                 holder.commentCount.text = hotTalkData[position].totalCommentCnt.toString()
                 holder.title.text = hotTalkData[position].text
+                holder.root.setOnClickListener {
+                    val intent = Intent(holder.itemView.context, QnaActivity::class.java)
+                    intent.putExtra(GlobalApplication.QNA_IDX, hotTalkData[position].idx)
+                    (holder.itemView.context as BaseActivity<*>).startActivityHorizontal(intent)
+                }
             }
             else -> {
                 (holder as HotTalkAbTestViewHolder).category.text = category[hotTalkData[position].categoryIdx]
                 holder.artistName.text = hotTalkData[position].text
                 Glide.with(holder.itemView.context).load(hotTalkData[position].imageUrl).into(holder.artistImage)
+                holder.root.setOnClickListener {
+                    val intent = Intent(holder.itemView.context, AbtestActivity::class.java)
+                    intent.putExtra(GlobalApplication.ABTEST_IDX, hotTalkData[position].idx)
+                    (holder.itemView.context as BaseActivity<*>).startActivityHorizontal(intent)
+                }
             }
         }
     }
