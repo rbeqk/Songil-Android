25415053ae18cc43969eb96ea51fae290a9a5f3d
YSH <sh10302@naver.com>
2022-02-12 20:22:53 +0900

    fix detail (add Job type variable)

--- app/src/main/java/com/example/songil/page_myfavorite_craft/MyFavoriteCraftActivity.kt
+++ app/src/main/java/com/example/songil/page_myfavorite_craft/MyFavoriteCraftActivity.kt
@@ -5,23 +5,28 @@ import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.PagingData
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.songil.R
 import com.example.songil.config.BaseActivity
+import com.example.songil.config.BaseViewModel
 import com.example.songil.databinding.MydetailActivityBinding
+import com.example.songil.popup_warning.SocketTimeoutDialog
 import com.example.songil.recycler.adapter.Craft1PagingAdapter
 import com.example.songil.recycler.decoration.Craft1Decoration
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 class MyFavoriteCraftActivity : BaseActivity<MydetailActivityBinding>(R.layout.mydetail_activity) {
 
     private val viewModel : MyFavoriteCraftViewModel by lazy { ViewModelProvider(this)[MyFavoriteCraftViewModel::class.java] }
+    private var pagingJob : Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding.tvTitle.text = "찜한 상품"
+        binding.tvTitle.text = getString(R.string.favorite_product)
         setRecyclerView()
         setObserver()
         setButton()
@@ -33,6 +38,11 @@ class MyFavoriteCraftActivity : BaseActivity<MydetailActivityBinding>(R.layout.m
         binding.layoutRefresh.setOnRefreshListener {
             viewModel.tryGetFavoriteCraftCnt()
         }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        viewModel.tryGetFavoriteCraftCnt()
     }
 
     private fun setRecyclerView(){
@@ -48,19 +58,49 @@ class MyFavoriteCraftActivity : BaseActivity<MydetailActivityBinding>(R.layout.m
                 binding.viewEmpty.root.visibility = View.VISIBLE
             } else {
                 binding.viewEmpty.root.visibility = View.GONE
-                lifecycleScope.launch {
-                    viewModel.flow.collectLatest { pagingData ->
-                        (binding.rvContent.adapter as Craft1PagingAdapter).submitData(pagingData)
-                    }
+                if ((binding.rvContent.adapter as Craft1PagingAdapter).itemCount > 0){
+                    (binding.rvContent.adapter as Craft1PagingAdapter).refresh()
+                } else {
+                    restartJob()
                 }
             }
         }
         viewModel.totalCnt.observe(this, cntObserver)
+
+        val errorObserver = Observer<BaseViewModel.FetchState>{ liveData ->
+            when(liveData){
+                BaseViewModel.FetchState.BAD_INTERNET -> {
+                    val timeOutDialog = SocketTimeoutDialog()
+                    timeOutDialog.show(supportFragmentManager, timeOutDialog.tag)
+                }
+                else -> {
+
+                }
+            }
+        }
+        viewModel.fetchState.observe(this, errorObserver)
     }
 
     private fun setButton(){
         binding.btnBack.setOnClickListener {
             onBackPressed()
+        }
+    }
+
+    private fun clearJob(){
+        pagingJob?.cancel()
+        pagingJob = lifecycleScope.launch {
+            (binding.rvContent.adapter as Craft1PagingAdapter).submitData(PagingData.empty())
+        }
+    }
+
+    private fun restartJob(){
+        pagingJob?.cancel()
+        pagingJob = lifecycleScope.launch {
+            (binding.rvContent.adapter as Craft1PagingAdapter).submitData(PagingData.empty())
+            viewModel.flow.collectLatest { pagingData ->
+                (binding.rvContent.adapter as Craft1PagingAdapter).submitData(pagingData)
+            }
         }
     }
 
