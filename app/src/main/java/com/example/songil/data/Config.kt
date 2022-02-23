b05d89ec9a9f7177a5d61ef56fc78c6470659309
YSH <sh10302@naver.com>
2022-02-23 20:04:44 +0900

    missed commit (change data variable name to fit in search delivery api)

--- app/src/main/java/com/example/songil/data/Config.kt
+++ app/src/main/java/com/example/songil/data/Config.kt
@@ -4,6 +4,6 @@ data class LikeData(val isLike : String, val totalLikeCnt : Int)
 
 data class PageCnt(val totalPages : Int, val itemsPerPage : Int)
 
-data class DeliveryStatus(val time : String, val currentPosition : String, val deliveryStatus : String)
+data class DeliveryStatus(val time : String, val where : String, val kind : String)
 
 data class UserType(val type : Int)
