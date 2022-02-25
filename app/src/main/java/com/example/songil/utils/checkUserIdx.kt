6662ed982561895570c9d70280bf267c347c0ca5
YSH <sh10302@naver.com>
2022-02-25 13:53:26 +0900

    remove unused part

--- app/src/main/java/com/example/songil/utils/checkUserIdx.kt
+++
@@ -1,7 +0,0 @@
-package com.example.songil.utils
-
-import com.example.songil.config.GlobalApplication
-
-fun checkUserIdx() : Boolean {
-    return (GlobalApplication.globalSharedPreferences.getInt(GlobalApplication.USER_IDX, 0) != 0)
-}
