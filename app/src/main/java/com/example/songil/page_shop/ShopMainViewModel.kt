4824d19eb8ee31ae83ea3fbf6dccb0e3a7ec426a
YSH <sh10302@naver.com>
2022-03-09 20:12:28 +0900

    remove unused part

--- app/src/main/java/com/example/songil/page_shop/ShopMainViewModel.kt
+++ app/src/main/java/com/example/songil/page_shop/ShopMainViewModel.kt
@@ -30,42 +30,4 @@ class ShopMainViewModel : BaseViewModel() {
             todayArtist.postValue(result.result.todayArtist)
         }
     }
-
-
-
-    /*private fun tempGetTodayArtists(){
-        val artistInfo = TodayArtistsResult(1, "이택", "https://cdn.pixabay.com/photo/2020/10/14/03/18/man-5653200_960_720.jpg", "금속 공예")
-        todayArtist.value = artistInfo
-    }
-
-    private fun tempGetNewCrafts(){
-        val newCraft = ArrayList<ClickData>()
-        newCraft.add(ClickData(1, "https://images.homify.com/c_fill,f_auto,q_0,w_740/v1439386593/p/photo/image/494903/IMG_0005_1_COEdit.jpg"))
-        newCraft.add(ClickData(1, "https://cdn.class101.net/images/07064f5a-c599-4c8b-b77a-a2c0857849ef/original"))
-        newCraft.add(ClickData(1, "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/2xEY/image/UVy3A7kOAnJVm_HaMW9vPWFyf-0.jpg"))
-        newCrafts.value = newCraft
-    }
-
-    private fun tempGetTodayCrafts(){
-        val todayCraft = ArrayList<Craft2>()
-        todayCraft.add(Craft2(1, "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/2xEY/image/UVy3A7kOAnJVm_HaMW9vPWFyf-0.jpg", "화병", "손길", 30000, "Y"))
-        todayCraft.add(Craft2(1, "https://cdn.class101.net/images/07064f5a-c599-4c8b-b77a-a2c0857849ef/original", "금속", "손길", 45000, "Y"))
-        todayCraft.add(Craft2(1, "https://images.homify.com/c_fill,f_auto,q_0,w_740/v1439386593/p/photo/image/494903/IMG_0005_1_COEdit.jpg", "접시", "손길", 55000, "Y"))
-        todayCraft.add(Craft2(1, "https://cdn.pixabay.com/photo/2018/03/29/18/50/diy-3273205_960_720.jpg", "손길", "손길", 30000, "Y"))
-        todayCrafts.value = todayCraft
-    }
-
-    private fun tempGetBannerData(){
-        val banner = ArrayList<String>()
-        banner.add("https://firebasestorage.googleapis.com/v0/b/dietmemory-65737.appspot.com/o/TempBanner%2Fbanner.png?alt=media&token=8d606c51-0bff-4b9a-aaa1-7582b963fbbd")
-        banner.add("https://firebasestorage.googleapis.com/v0/b/dietmemory-65737.appspot.com/o/TempBanner%2Fbanner.png?alt=media&token=8d606c51-0bff-4b9a-aaa1-7582b963fbbd")
-        bannerData.value = banner
-    }
-
-    fun loadData(){
-        tempGetTodayArtists()
-        tempGetNewCrafts()
-        tempGetTodayCrafts()
-        tempGetBannerData()
-    }*/
 }
