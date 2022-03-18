d58ad5a8bed31eb8ede9d8973810d43ee75be22f
YSH <sh10302@naver.com>
2022-03-18 16:48:46 +0900

    add view that shows comment cnt is 0

--- app/src/main/java/com/songil/songil/page_craft/subpage_comment/CraftFragmentComment.kt
+++ app/src/main/java/com/songil/songil/page_craft/subpage_comment/CraftFragmentComment.kt
@@ -14,7 +14,7 @@ import com.songil.songil.recycler.adapter.CraftCommentAdapter
 import com.songil.songil.recycler.decoration.CraftCommentDecoration
 import com.songil.songil.utils.dpToPx
 
-class CraftFragmentComment() : BaseFragment<CraftFragmentReviewBinding>(CraftFragmentReviewBinding::bind, R.layout.craft_fragment_review) {
+class CraftFragmentComment : BaseFragment<CraftFragmentReviewBinding>(CraftFragmentReviewBinding::bind, R.layout.craft_fragment_review) {
 
     private val viewModel : CraftCommentViewModel by lazy { ViewModelProvider(this)[CraftCommentViewModel::class.java] }
 
@@ -27,6 +27,8 @@ class CraftFragmentComment() : BaseFragment<CraftFragmentReviewBinding>(CraftFra
 
         setRecyclerView()
         setObserver()
+
+        setEmptyText()
 
         fitLayoutHeight()
     }
@@ -44,13 +46,17 @@ class CraftFragmentComment() : BaseFragment<CraftFragmentReviewBinding>(CraftFra
         viewModel.photoOnly.observe(viewLifecycleOwner, isPhotoObserver)
 
         val pageCntObserver = Observer<Int>{ liveData ->
+            (binding.rvReview.adapter as CraftCommentAdapter).clearData()
             if (liveData > 0){
-                (binding.rvReview.adapter as CraftCommentAdapter).clearData()
+                binding.viewEmpty.root.visibility = View.GONE
                 if (liveData == 1) {
                     viewModel.tryGetComments()
                 } else {
                     viewModel.tryGetCommentFirst()
                 }
+            } else {
+                setEmptyText()
+                binding.viewEmpty.root.visibility = View.VISIBLE
             }
         }
         viewModel.craftCommentPageCnt.observe(viewLifecycleOwner, pageCntObserver)
@@ -71,5 +77,10 @@ class CraftFragmentComment() : BaseFragment<CraftFragmentReviewBinding>(CraftFra
 
     private fun fitLayoutHeight(){
         binding.layoutMain.minHeight = getWindowSize()[1] - (activity as CraftActivity).getToolbarHeight() - getStatusBarHeight() - dpToPx(requireContext(), 56)
+    }
+
+    // 빈 화면에서 표기될 text 설정, 포토 코멘트만 보기일 경우와 아닌 경우가 구분되어 있다.
+    private fun setEmptyText(){
+        binding.viewEmpty.tvEmptyTarget.text = if (viewModel.photoOnly.value == true) getString(R.string.empty_craft_comment_photo) else getString(R.string.empty_craft_comment)
     }
 }
