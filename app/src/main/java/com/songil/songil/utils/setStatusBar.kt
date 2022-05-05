8efa6c8daca7f026d0cc05bfbc9a1ff4b894cd7f
YSH <sh10302@naver.com>
2022-05-05 13:20:30 +0900

    안드로이드 12 이상에서 status bar의  text 색상이 반대로 나오던 문제 수정

--- app/src/main/java/com/songil/songil/utils/setStatusBar.kt
+++ app/src/main/java/com/songil/songil/utils/setStatusBar.kt
@@ -8,6 +8,7 @@ import android.view.WindowManager
 import androidx.core.content.ContextCompat
 import com.songil.songil.R
 
+@Suppress("UsePropertyAccessSyntax", "DEPRECATION")
 fun setStatusBarBlack(activity : Activity, isDark : Boolean){
     val window = activity.window
     window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
@@ -15,7 +16,7 @@ fun setStatusBarBlack(activity : Activity, isDark : Boolean){
         window.statusBarColor = ContextCompat.getColor(activity, R.color.songil_2)
         when {
             Build.VERSION.SDK_INT >= 31 -> {
-                window.insetsController?.setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
+                window.insetsController?.setSystemBarsAppearance(0, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
             }
             Build.VERSION.SDK_INT >= 26 -> {
                 window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR)
@@ -29,10 +30,10 @@ fun setStatusBarBlack(activity : Activity, isDark : Boolean){
             }
         }
     } else {
-        //window.statusBarColor = ContextCompat.getColor(activity, R.color.songil_1)
+        window.statusBarColor = ContextCompat.getColor(activity, R.color.songil_1)
         when {
             Build.VERSION.SDK_INT >= 31 -> {
-                window.insetsController?.setSystemBarsAppearance(0, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
+                window.insetsController?.setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
             }
             Build.VERSION.SDK_INT >= 26 -> {
                 /*val windowInsetsController = ViewCompat.getWindowInsetsController(activity.window.decorView)
@@ -41,7 +42,6 @@ fun setStatusBarBlack(activity : Activity, isDark : Boolean){
                 //window.clearFlags(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
                 window.decorView.setSystemUiVisibility(window.decorView.getSystemUiVisibility() and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv())
                 window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
-                window.statusBarColor = ContextCompat.getColor(activity, R.color.songil_1)
             }
             else -> {
                 window.clearFlags(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
